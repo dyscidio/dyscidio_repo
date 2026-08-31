ee5f8e43f87933bab1b7a6cc071ed1894dad7b6e
dyscidio <dyscidio@protonmail.com>
2026-08-31 17:38:05 +0200

    Update main.py

--- plugin.video.dyscidio/main.py
+++ plugin.video.dyscidio/main.py
@@ -1,29 +1,49 @@
 import xbmcgui
 import xbmcplugin
+import xbmcaddon
 import sys
+import os
 
 HANDLE = int(sys.argv[1])
 BASE_URL = sys.argv[0]
 
+ADDON = xbmcaddon.Addon()
+ADDON_PATH = ADDON.getAddonInfo("path")
 
-def add_folder(name, params):
-    url = BASE_URL + "?" + "&".join(
+MEDIA_PATH = os.path.join(ADDON_PATH, "resources", "media")
+
+
+def build_url(params):
+    return BASE_URL + "?" + "&".join(
         f"{key}={value}" for key, value in params.items()
     )
 
+
+def add_folder(name, icon):
     item = xbmcgui.ListItem(label=name)
+
+    item.setArt({
+        "thumb": icon,
+        "icon": icon
+    })
 
     xbmcplugin.addDirectoryItem(
         handle=HANDLE,
-        url=url,
+        url=build_url({"folder": "news"}),
         listitem=item,
         isFolder=True
     )
 
 
-def add_stream(name, url):
+def add_stream(name, url, icon):
     item = xbmcgui.ListItem(label=name)
+
     item.setProperty("IsPlayable", "true")
+
+    item.setArt({
+        "thumb": icon,
+        "icon": icon
+    })
 
     xbmcplugin.addDirectoryItem(
         handle=HANDLE,
@@ -33,28 +53,48 @@ def add_stream(name, url):
     )
 
 
-params = dict()
+# MENÚ PRINCIPAL
 
-if len(sys.argv) > 2 and sys.argv[2]:
+if len(sys.argv) < 3 or not sys.argv[2]:
+
+    news_icon = os.path.join(MEDIA_PATH, "cnn.png")
+
+    add_folder("News", news_icon)
+
+    xbmcplugin.endOfDirectory(HANDLE)
+
+
+# NEWS
+
+else:
+
     params = dict(
         pair.split("=", 1)
         for pair in sys.argv[2][1:].split("&")
         if "=" in pair
     )
 
+    if params.get("folder") == "news":
 
-# MENÚ PRINCIPAL
-if not params:
-    add_folder("News", {"folder": "news"})
-    xbmcplugin.endOfDirectory(HANDLE)
+        # CNN
+        add_stream(
+            "CNN International",
+            "URL_CNN_AQUI",
+            os.path.join(MEDIA_PATH, "cnn.png")
+        )
 
+        # CBS News
+        add_stream(
+            "CBS News 24/7",
+            "https://cbsn-us.cbsnstream.cbsnews.com/out/v1/55a8648e8f134e82a470f83d562deeca/master.m3u8",
+            os.path.join(MEDIA_PATH, "cbs.png")
+        )
 
-# NEWS
-elif params.get("folder") == "news":
+        # Euronews
+        add_stream(
+            "Euronews Español",
+            "URL_EURONEWS_AQUI",
+            os.path.join(MEDIA_PATH, "euronews.png")
+        )
 
-    add_stream(
-        "CBS News 24/7",
-        "https://cbsn-us.cbsnstream.cbsnews.com/out/v1/55a8648e8f134e82a470f83d562deeca/master.m3u8"
-    )
-
-    xbmcplugin.endOfDirectory(HANDLE)
+        xbmcplugin.endOfDirectory(HANDLE)
