b89b9727aef11079e46b413ef16b90d3e5458380
dyscidio <dyscidio@protonmail.com>
2026-08-31 17:14:17 +0200

    Refactor link addition to use add_folder function

--- plugin.video.dyscidio/main.py
+++ plugin.video.dyscidio/main.py
@@ -3,11 +3,28 @@ import xbmcplugin
 import sys
 
 HANDLE = int(sys.argv[1])
+BASE_URL = sys.argv[0]
 
 
-def add_link(name, url):
+def add_folder(name, params):
+    url = BASE_URL + "?" + "&".join(
+        f"{key}={value}" for key, value in params.items()
+    )
+
+    item = xbmcgui.ListItem(label=name)
+
+    xbmcplugin.addDirectoryItem(
+        handle=HANDLE,
+        url=url,
+        listitem=item,
+        isFolder=True
+    )
+
+
+def add_stream(name, url):
     item = xbmcgui.ListItem(label=name)
     item.setProperty("IsPlayable", "true")
+
     xbmcplugin.addDirectoryItem(
         handle=HANDLE,
         url=url,
@@ -16,8 +33,28 @@ def add_link(name, url):
     )
 
 
-add_link("Google", "https://www.google.com")
-add_link("Wikipedia", "https://www.wikipedia.org")
-add_link("GitHub", "https://github.com")
+params = dict()
 
-xbmcplugin.endOfDirectory(HANDLE)
+if len(sys.argv) > 2 and sys.argv[2]:
+    params = dict(
+        pair.split("=", 1)
+        for pair in sys.argv[2][1:].split("&")
+        if "=" in pair
+    )
+
+
+# MENÚ PRINCIPAL
+if not params:
+    add_folder("News", {"folder": "news"})
+    xbmcplugin.endOfDirectory(HANDLE)
+
+
+# NEWS
+elif params.get("folder") == "news":
+
+    add_stream(
+        "CBS News 24/7",
+        "https://cbsn-us.cbsnstream.cbsnews.com/out/v1/55a8648e8f134e82a470f83d562deeca/master.m3u8"
+    )
+
+    xbmcplugin.endOfDirectory(HANDLE)
